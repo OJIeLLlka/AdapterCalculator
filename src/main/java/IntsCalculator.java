e9c9a4c6ab3e38e854789376d50c0615bb814fa5
OJIeLLlka <olbastnie@gmail.com>
2022-04-12 17:52:56 +0300

    fixed calling methods in sum, mult, pow

--- src/main/java/IntsCalculator.java
+++ src/main/java/IntsCalculator.java
@@ -8,18 +8,30 @@ public class IntsCalculator implements Ints {
     @Override
     public int sum(int arg0, int arg1) {
         //считаем через target
-        return (int) target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.SUM).result();
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.SUM)
+                .result();
     }
 
     @Override
     public int mult(int arg0, int arg1) {
         //считаем через target
-        return (int) target.newFormula().addOperand(arg0).addOperand(arg1).calculate(Calculator.Operation.MULT).result();
+        return (int) target.newFormula()
+                .addOperand(arg0)
+                .addOperand(arg1)
+                .calculate(Calculator.Operation.MULT)
+                .result();
     }
 
     @Override
     public int pow(int a, int b) {
         //считаем через target
-        return (int) target.newFormula().addOperand(a).addOperand(b).calculate(Calculator.Operation.POW).result();
+        return (int) target.newFormula()
+                .addOperand(a)
+                .addOperand(b)
+                .calculate(Calculator.Operation.POW)
+                .result();
     }
 }
